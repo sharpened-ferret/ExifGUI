d1d4833d15e2dd1a465c06d355ca6f118a664a26
Neil Woodhouse <neiltwoodhouse@gmail.com>
2023-03-16 15:08:18 +0000

    Started seperated per-file-type formatting

--- exif_tool/lib/utils.dart
+++ exif_tool/lib/utils.dart
@@ -12,5 +12,32 @@ ImageProvider getImageFromFile(File? f) {
 
 String generateExifDisplayString(Map jsonData) {
   String returnText = "File: ${jsonData['FileName']}\nFile Type: ${jsonData['FileType']}\nFile Size: ${jsonData['FileSize']}\n";
+
+  final String FILE_TYPE = jsonData['MIMEType'].split('/')[1];
+
+  switch (FILE_TYPE) {
+    case "png": {
+      returnText = """${returnText}Image Width: ${jsonData['ImageWidth']}
+Image Height: ${jsonData['ImageHeight']}
+""";
+    }
+    break;
+
+    case "jpeg": {
+      debugPrint("jpeg detected");
+      returnText = """${returnText}Image Width: ${jsonData['ImageWidth']}
+Image Height: ${jsonData['ImageHeight']}
+""";
+    }
+    break;
+
+    default: {
+      returnText = "Unknown File Type Detected!\nUsing Default Handling\n";
+
+      jsonData.forEach((key, value) {
+        returnText = "${returnText}\n$key: $value";
+      });
+    }
+  }
   return returnText;
 }
